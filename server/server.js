36a2338945244b86b9fac847640e24541934ce91
Nicholas Tinsley <n_tinsley@hotmail.com>
2016-11-24 12:01:11 -0600

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -115,7 +115,7 @@ app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => { 
+    return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
     });
   }).catch((e) => {
@@ -124,6 +124,13 @@ app.post('/users/login', (req, res) => {
 
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
